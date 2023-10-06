f2bdbee4b5820b609ef47efe954109e1ac8f1bc0
seguna15 <seguna15@gmail.com>
2023-10-06 10:47:00 +0100

    home page and about page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header';
+import Home from '../components/Home';
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 }
